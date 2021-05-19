a3a00f44989670154d7a5d1cbf8862a1cfdac907
Abhishek sahu <sahuabhishek42000@gmail.com>
2021-05-19 18:01:21 +0530

    Security

--- src/main/java/com/divergentsl/security1/SpringSecurity1Application.java
+++ src/main/java/com/divergentsl/security1/SpringSecurity1Application.java
@@ -1,13 +1,49 @@
 package com.divergentsl.security1;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringSecurity1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurity1Application.class, args);
+	}
+
+	private Connector httpToHttpsRedirectConnector() {
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("http");
+		connector.setPort(8082);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
+	}
+
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		// Enable SSL Traffic
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+
+		// Add HTTP to HTTPS redirect
+		tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+		return tomcat;
 	}
 
 }
